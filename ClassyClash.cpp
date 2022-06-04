0006f91d2d2fda6bd5f7ad823a2bf33fe0f45495
Sergio Prada <sergioeprada@hotmail.com>
2022-06-04 00:15:16 -0500

    Progress until completing video 63

--- ClassyClash.cpp
+++ ClassyClash.cpp
@@ -6,6 +6,14 @@ static const Vector2 windowDimensions{384, 384};
 static const char* window_title = "Classy Clasher";
 static const Color backgroundColor = BLACK;
 
+class Character
+{
+public:
+    Character();
+    ~Character();
+private:
+};
+
 int main()
 {
     InitWindow(windowDimensions.x, windowDimensions.y, window_title);
@@ -15,19 +23,26 @@ int main()
     Vector2 mapPos {0.0f, 0.0f};
     float speed{4};
 
-    Texture2D knight = LoadTexture("Resources/characters/knight_idle_spritesheet.png");
+    Texture2D knightIdle = LoadTexture("Resources/characters/knight_idle_spritesheet.png");
+    Texture2D knightRun = LoadTexture("Resources/characters/knight_run_spritesheet.png");
     Vector2 knightPos
     {
-        ((float)windowDimensions.x/2.0f) - (4.0f * (0.5f * (float)knight.width / 6.0f)),
-        ((float)windowDimensions.y/2.0f) - (4.0f * (0.5f * (float)knight.height)) 
+        ((float)windowDimensions.x/2.0f) - (4.0f * (0.5f * (float)knightIdle.width / 6.0f)),
+        ((float)windowDimensions.y/2.0f) - (4.0f * (0.5f * (float)knightIdle.height)) 
     };
     Rectangle knightDest 
     {
         knightPos.x,
         knightPos.y,
-        (float)knight.width/6.0f*4.0f,
-        (float)knight.height*4.0f
+        (float)knightIdle.width/6.0f*4.0f,
+        (float)knightIdle.height*4.0f
     };
+    float knightRightLeft{1.f};
+    float knightRunningTime{};
+    int knightFrame{};
+    const int knightMaxFrames{6};
+    const float knightUpdateTime{1.f/12.f}; //Update the knightIdle 12 times per frame
+
     while (!WindowShouldClose())
     {
         BeginDrawing();
@@ -38,25 +53,36 @@ int main()
             if (IsKeyDown(KEY_D)) direction.x += 1.0;
             if (IsKeyDown(KEY_W)) direction.y -= 1.0;
             if (IsKeyDown(KEY_S)) direction.y += 1.0;
-            if(Vector2Length(direction) != 0.0)
+            
+            float directionMagnitude = Vector2Length(direction); 
+            if(directionMagnitude != 0.0)
             {
                 direction = Vector2Scale(Vector2Normalize(direction), speed);
                 mapPos = Vector2Subtract(mapPos, direction);
+                knightRightLeft = direction.x<0 ? -1.f : 1.f;
             }
             DrawTextureEx(map, mapPos, 0.0f, 4.0f, WHITE);
+            knightRunningTime += GetFrameTime();
+            if(knightRunningTime>knightUpdateTime)
+            {
+                knightFrame++;
+                knightRunningTime = 0;
+                if(knightFrame>knightMaxFrames) knightFrame = 0;
+            }
             Rectangle knightSouce 
             {
+                knightFrame * knightRightLeft * (float)knightIdle.width/6.0f,
                 0.0f,
-                0.0f,
-                (float)knight.width/6.0f,
-                (float)knight.height
+                knightRightLeft * (float)knightIdle.width/6.0f,
+                (float)knightIdle.height
             };
-            DrawTexturePro(knight, knightSouce, knightDest, Vector2{}, 0.0f, WHITE);
+            DrawTexturePro(directionMagnitude ? knightRun : knightIdle, knightSouce, knightDest, Vector2{}, 0.0f, WHITE);
         EndDrawing();
         glFinish();
     }
     UnloadTexture(map);
-    UnloadTexture(knight);
+    UnloadTexture(knightIdle);
+    UnloadTexture(knightRun);
     CloseWindow();
     return 0;
 }
